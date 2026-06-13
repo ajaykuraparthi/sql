2c6a189b16927d9bdf4bae087bcdd3f48459d7d3
Ajay8179 <kuraparthiajay@gmail.com>
2026-06-14 00:30:41 +0530

    added files of joins

--- JOINS.sql
+++ JOINS.sql
@@ -3,9 +3,18 @@
 -- JOINS
 
 -- INNER JOIN
+							-- for joining the the two tables based on on the common attribute name
+SELECT *     -- select table
+FROM cust		-- from the cust table
+INNER JOIN payments		-- join with the tbl payments
+  ON payments.customerNumber = cust.custno;   -- payment tables custNo attribute and cust table's cust no attrubute
 
-SELECT *
+-- if i want to select only the particular colums do this
+
+SELECT cust.custno,
+	  name,
+      payments.amount
 FROM cust
 INNER JOIN payments
-  ON payments.customerNumber = cust.custno;
-  
+ON payments.customerNumber = cust.custno;
+
